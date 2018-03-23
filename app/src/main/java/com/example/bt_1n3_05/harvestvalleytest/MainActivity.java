4dbca0700985875fe6b14d236d0321c794cc8b6f
BT_1N3_05 <jwu4227@bths.edu>
2018-03-23 11:13:54 -0400

    ...

--- app/src/main/java/com/example/bt_1n3_05/harvestvalleytest/MainActivity.java
+++ app/src/main/java/com/example/bt_1n3_05/harvestvalleytest/MainActivity.java
@@ -9,28 +9,28 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FloatingActionButton newButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+       newButton = (FloatingActionButton) findViewById(R.id.newButton);
+       newButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               openChooseGenderScreen();
+           }
+       });
     }
 
-    public void farmScreen(View view){
-        Intent startNewActivity = new Intent(this, FarmScreen.class);
+    public void openChooseGenderScreen(){
+        Intent intent = new Intent(this, ChooseGenderScreen.class);
+        startActivity(intent);
     }
 
     @Override
@@ -55,3 +55,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
