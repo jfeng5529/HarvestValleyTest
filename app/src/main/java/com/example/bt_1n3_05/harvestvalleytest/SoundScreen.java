4d87ca95b7ded690dcf8b122e8bb947f65fe2e72
BT_1N3_02 <jfeng5529@bths.edu>
2018-04-11 11:33:04 -0400

    i did some backendd

--- app/src/main/java/com/example/bt_1n3_05/harvestvalleytest/SoundScreen.java
+++ app/src/main/java/com/example/bt_1n3_05/harvestvalleytest/SoundScreen.java
@@ -2,12 +2,19 @@ package com.example.bt_1n3_05.harvestvalleytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class SoundScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sound_screen);
+        setContentView(R.layout.inventory2);
+    }
+
+    public void onButtonItems(View v){
+        Toast test = Toast.makeText(getApplicationContext(), "HEYY!", Toast.LENGTH_LONG);
+        test.show();
     }
 }
