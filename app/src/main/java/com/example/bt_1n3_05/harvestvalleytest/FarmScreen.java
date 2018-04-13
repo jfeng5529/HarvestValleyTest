6753aea6207d6ec1131d0c81400c94fbdecc0643
BT_1N3_05 <jwu4227@bths.edu>
2018-04-13 11:09:21 -0400

    ...

---
+++ app/src/main/java/com/example/bt_1n3_05/harvestvalleytest/FarmScreen.java
@@ -0,0 +1,17 @@
+package com.example.bt_1n3_05.harvestvalleytest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by BT_1N3_05 on 4/13/2018.
+ */
+
+public class FarmScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.farm);
+    }
+}
+
