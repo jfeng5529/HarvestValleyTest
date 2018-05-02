630af3a7102ede361d818fe4f14317e47240e200
BT_1N3_05 <jwu4227@bths.edu>
2018-05-02 11:27:01 -0400

    helloOoOOoOoo

--- app/src/main/java/com/example/bt_1n3_05/harvestvalleytest/FarmScreen.java
+++ app/src/main/java/com/example/bt_1n3_05/harvestvalleytest/FarmScreen.java
@@ -12,6 +12,7 @@ public class FarmScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.farm);
+
     }
 }
 
