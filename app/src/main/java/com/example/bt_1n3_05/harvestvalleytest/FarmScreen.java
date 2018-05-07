9de26bc3898d963ae8cb4fb0f9a6963354c4eefb
BT_1N3_03 <lkhalid0095@bths.edu>
2018-05-07 11:37:43 -0400

    GIFS NOT WORKING

--- app/src/main/java/com/example/bt_1n3_05/harvestvalleytest/FarmScreen.java
+++ app/src/main/java/com/example/bt_1n3_05/harvestvalleytest/FarmScreen.java
@@ -3,6 +3,9 @@ package com.example.bt_1n3_05.harvestvalleytest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+
+import pl.droidsonroids.gif.GifImageView;
+
 /**
  * Created by BT_1N3_05 on 4/13/2018.
  */
@@ -13,6 +16,10 @@ public class FarmScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.farm);
 
+        GifImageView gifImageView = (GifImageView) findViewById(R.id.GifImageView);
+//        gifImageView.setGifImageResource(R.drawable.doggie);
+        }
     }
-}
+
+
 
